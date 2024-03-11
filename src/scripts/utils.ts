8e31be020b7c5d26c34e23495a5e95e13f70ddac
fenroe <scrambles9494@gmail.com>
2024-03-11 23:43:29 +0200

    utils updated

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -14,3 +14,45 @@ export function formatDate(date: Date) {
     timeZone: "UTC",
   });
 }
+
+interface FormatBlogPostsOptions {
+  filterOutDrafts?: boolean;
+  filterOutFuturePosts?: boolean;
+  sortByDate?: boolean;
+  limit?: number;
+}
+
+export function formatBlogPosts(
+  posts: any[],
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  }: FormatBlogPostsOptions = {}
+) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    // filter out drafts if true
+    if (filterOutDrafts && draft) return acc;
+    // filter out future posts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a: any, b: any) =>
+        (new Date(b.frontmatter.date) as any) -
+        (new Date(a.frontmatter.date) as any)
+    );
+  }
+
+  if (limit) {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
+}
